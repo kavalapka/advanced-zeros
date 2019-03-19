df1ac46373a47af0dcc6b20d0b171a74f22cc29b
Katerina Zenchik <e.zenchik@gmail.com>
2019-03-20 01:12:34 +0300

    add zeros count function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+
+  let base_factor = {};
+
+  for (let i = 2; i <= base; i++) {
+    let count  = 0;
+    while (base % i === 0) {
+      base = base / i;
+      count ++;
+      base_factor[i] = count;
+    }
+  }
+
+  let power_of_factor = [];
+
+  for (let key in base_factor) {
+    let num = number;
+    let sum = 0;
+    while (num) {
+      num = Math.floor(num/key);
+      sum += num;
+    }
+    power_of_factor.push(Math.floor(sum/base_factor[key]));
+  }
+
+  return Math.min(...power_of_factor);
+};
